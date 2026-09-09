4b8b0dfbab39465aa48c825ae82048511206dbaa
martabehun1807 <martabehun18@gmail.com>
2026-09-09 20:21:49 +0300

    Add 4 tasks to jsbasics

---
+++ jsBasics/task2.js
@@ -0,0 +1,8 @@
+const name1 = 'Alex';
+const name2 = 'Anna'; 
+
+const greeting1 = 'Hello, ' + name1 +  ' and ' + name2 ;
+console.log (greeting1)
+
+const greeting2 = `Hello, ${name1} and ${name2}`;
+console.log (greeting2)
---
+++ jsBasics/task4.js
@@ -0,0 +1,13 @@
+const radius = 6
+const Circle_area = Math.PI * radius ** 2
+console.log (Circle_area.toFixed(2))
+
+const length = 12
+const width = 6
+const Rectangle_area = length * width
+console.log (Rectangle_area.toFixed(2))
+
+const radius2 = 7
+const height = 4
+const Cylinder_volume = Math.PI * radius2 ** 2 * height
+console.log (Cylinder_volume.toFixed(2))
