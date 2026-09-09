75f40462b3db469ac304fd6e899933c7d97d3efc
martabehun1807 <martabehun18@gmail.com>
2026-09-09 22:08:08 +0300

    Update gitignore

--- jsBasics/task2.js
+++
@@ -1,8 +0,0 @@
-const name1 = 'Alex';
-const name2 = 'Anna'; 
-
-const greeting1 = 'Hello, ' + name1 +  ' and ' + name2 ;
-console.log (greeting1)
-
-const greeting2 = `Hello, ${name1} and ${name2}`;
-console.log (greeting2)
