2feb1cf62f377bad372841a383e0246e5c0eaa26
martabehun1807 <martabehun18@gmail.com>
2026-09-17 23:56:19 +0300

    Add tasks lesson 6

---
+++ lesson 6/task1.js
@@ -0,0 +1,12 @@
+function square(width,height){
+    return width * height;
+}
+console.log(square(5,10));
+
+const square2 = function (width,height) {
+    return width * height;
+}
+console.log(square2(5,10));
+
+const square3 = (width, height) => width * height
+console.log(square3(5,10));
